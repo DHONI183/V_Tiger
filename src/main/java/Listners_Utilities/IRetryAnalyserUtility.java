8eaee92335c5515e8dfad9a303ff1a663d89fe8b
Manish Kumar Verma <117851676+DHONI183@users.noreply.github.com>
2025-07-31 17:10:26 +0530

    Update IRetryAnalyserUtility.java

--- src/main/java/Listners_Utilities/IRetryAnalyserUtility.java
+++ src/main/java/Listners_Utilities/IRetryAnalyserUtility.java
@@ -3,7 +3,7 @@ package Listners_Utilities;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
-
+// changes for github
 public class IRetryAnalyserUtility implements IRetryAnalyzer{
 
 	int max = 5;
